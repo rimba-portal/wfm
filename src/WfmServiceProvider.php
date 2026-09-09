57b2148dc5d868c956117855be4f22544a6c7e10
Faros <114036707+a-faros@users.noreply.github.com>
2026-09-09 15:43:50 +0800

    Release v1.4.8

--- src/WfmServiceProvider.php
+++ src/WfmServiceProvider.php
@@ -42,7 +42,7 @@ class WfmServiceProvider extends BitesServiceProvider
         $commands = [];
         foreach (glob($commandDir.'/*.php') as $file) {
             $className = basename($file, '.php');
-            $class = 'Rimba\\Base\\Console\\Commands\\'.$className;
+            $class = 'Rimba\\Wfm\\Console\\Commands\\'.$className;
             if (class_exists($class) && is_subclass_of($class, Command::class)) {
                 $reflection = new ReflectionClass($class);
                 if (! $reflection->isAbstract()) {
